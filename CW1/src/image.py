261eac234c2dbcb222a520cd81ea8f8bf98ad859
Michele O'Neill <micheleevaone@gmail.com>
2016-11-09 23:31:32 +0000

    adding html, css, png and js files

--- CW1/src/image.py
+++ CW1/src/image.py
@@ -1,10 +1,6 @@
 from flask import Flask , url_for , abort, request
 app = Flask ( __name__ )
 
-@app.route("/")
-def hello():
-	return "Hello napier"
-
 @app.route('/static-example/img')
 def static_example_img():
 	start = '<img src="'
@@ -19,7 +15,27 @@ def account():
 	else:
 		return "method = GET"
 
-@app.route()
+@app.route("/login/", methods=['POST','GET'])
+def login():
+	if request.method == 'POST':
+		print request.form
+		name = request.form['name']
+		return "Hello %s" % name
+	else:
+		page = '''
+		<html><body>
+			<form action="" method="post" name="form">
+				<label for="name">Name:</label>
+				<input type="text" name="name" id="name"/>
+				<input type="submit" name="submit" id="submit"/>
+			</form>
+		</body><html>'''
+
+		return page
+
+@app.route("/hello/<name>")
+def hello(name):
+	return "Hello %s" % name
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
