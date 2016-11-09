e6de0423d9eb1459c2ac522cdc8ce846729b24c6
Michele O'Neill <micheleevaone@gmail.com>
2016-11-09 23:31:32 +0000

    adding html, css, png and js files

---
+++ CW1/src/static/js/menu_html_block.js
@@ -0,0 +1,14 @@
+// <![CDATA[
+document.writeln('<ul class="menusm" id="menu-top-menu">');
+document.writeln('  <li><a href="index.html"><span>Home</span></a>');
+document.writeln('  </li>');
+
+document.writeln('  <li><a href="music-videos.html"><span>Music Videos</span></a>');
+document.writeln('  </li>');
+
+document.writeln('  <li><a href="singles.html"><span>Singles</span></a>');
+document.writeln('  </li>');
+
+document.writeln('</ul>');
+document.writeln('<div class="clr"></div>');
+// ]]>
