a872c05e748a857122b0bd1a4c70be810461c70a
Michele O'Neill <micheleevaone@gmail.com>
2016-11-10 05:34:45 +0000

    updating html

--- CW1/src/static/js/menu_html_block.js
+++ CW1/src/static/js/menu_html_block.js
@@ -1,12 +1,12 @@
 // <![CDATA[
 document.writeln('<ul class="menusm" id="menu-top-menu">');
-document.writeln('  <li><a href="index.html"><span>Home</span></a>');
+document.writeln('  <li><a href="/"><span>Home</span></a>');
 document.writeln('  </li>');
 
-document.writeln('  <li><a href="music-videos.html"><span>Music Videos</span></a>');
+document.writeln('  <li><a href="/musicvideos"><span>Music Videos</span></a>');
 document.writeln('  </li>');
 
-document.writeln('  <li><a href="singles.html"><span>Singles</span></a>');
+document.writeln('  <li><a href="/singles"><span>Singles</span></a>');
 document.writeln('  </li>');
 
 document.writeln('</ul>');
