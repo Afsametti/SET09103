7266d0d22ea9aab65d53ff42def87776d8d7ca7b
Michele O'Neill <micheleevaone@gmail.com>
2016-11-10 05:34:45 +0000

    updating html

--- CW1/src/run.py
+++ CW1/src/run.py
@@ -1,29 +1,27 @@
 import os
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 #Homepage
 @app.route("/")
-def index():
-  return render_template('index.html', title='Home')
+def home():
+  return render_template('index.html', title='Home'), 200
 
 #Singles
 @app.route('/singles')
-def comedies():
-  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-  return render_template('singles.html', title='Singles')
+def singles():
+  return render_template('singles.html', title='Singles'), 200
 
 #Music Videos
-@app.route('/music-videos/')
-def drama():
-  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-  return render_template('music-videos.html', title='Music Videos')
+@app.route('/musicvideos')
+def musicvideos():
+  return render_template('music-videos.html', title='Music Videos'), 200
 
 
-#Error page
+#Error page 404
 @app.errorhandler (404)
 def page_not_found (error):
   return render_template('404.html', title='Error'), 404
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=False)
+  app.run(host='0.0.0.0', debug=True)
