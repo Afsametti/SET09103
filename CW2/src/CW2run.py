06839a31195f20d151f737b4ef8a2930f69a480f
Michele O'Neill <micheleevaone@gmail.com>
2016-11-17 21:12:50 +0000

    created CW2 folders and some html files

---
+++ CW2/src/CW2run.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+#Homepage
+@app.route("/")
+def index():
+	return render_template('index.html', title='Mies fanpage | Home'), 200
+
+#Contact
+@app.route('/contact/')
+def mies_contact():
+	return render_template('contact.html', title='Mies fanpage | Contact'), 200
+
+#Error page 404
+@app.errorhandler (404)
+def page_not_found (error):
+	return render_template('404.html', title='Mies fanpage | Error'), 404
+
+if __name__ == "__main__":
+ 	app.run(host='0.0.0.0', debug=True)
---
+++ CW2/src/static/js/menu_html_block.js
@@ -0,0 +1,39 @@
+// <![CDATA[
+
+document.writeln('<ul id="nav" class="sf-menu">');
+document.writeln('	<li><a href="index.html" class="menu_navigation"><span>Home</span></a>');
+document.writeln('	</li>');
+
+document.writeln('	<li><a href="#" class="menu_navigation"><span>About Ludwig Mies van der Rohe</span></a>');
+document.writeln('  </li>');
+
+document.writeln('  <li><a href="portfolio-1.html" class="menu_navigation"><span>Portfolio</span></a>');
+document.writeln('    <ul>');
+document.writeln('  <li><a href="#" class="menu_navigation"><span>Buildings >></span></a>');
+document.writeln('    <ul>');
+document.writeln('      <li><a href="portfolio-1.html" class="menu_navigation"><span>Residential buildings</span></a></li>');
+document.writeln('      <li><a href="portfolio-2.html" class="menu_navigation"><span>Public buildings</span></a></li>');
+document.writeln('      <li><a href="portfolio-3.html" class="menu_navigation"><span>Unrealised projects</span></a></li>');
+document.writeln('    </ul>');
+document.writeln('  </li>');
+document.writeln('      <li><a href="portfolio_iso.html" class="menu_navigation"><span>Forniture</span></a></li>');
+document.writeln('    </ul>');
+document.writeln('  </li>');
+
+document.writeln('  <li><a href="gallery.html" class="menu_navigation"><span>Gallery</span></a>');
+document.writeln('    <ul>');
+document.writeln('      <li><a href="gallery.html" class="menu_navigation"><span>Buildings</span></a></li>');
+document.writeln('      <li><a href="gallery/desktop-gallery/index.html" class="menu_navigation"><span>Forniture</span></a></li>');
+document.writeln('      <li><a href="gallery/multimedia-gallery/index.html" class="menu_navigation"><span>Drawings</span></a></li>');
+document.writeln('      <li><a href="gallery/multimedia-gallery/index.html" class="menu_navigation"><span>Wallpapers</span></a></li>');
+document.writeln('    </ul>');
+document.writeln('  </li>');
+
+document.writeln('  <li><a href="gallery.html" class="menu_navigation"><span>Interests</span></a>');
+document.writeln('  </li>');
+
+document.writeln('  <li><a href="contact.html" class="menu_navigation"><span>Contact</span></a></li>');
+document.writeln('</ul>');
+document.writeln('<div class="clr"></div>');
+
+// ]]>
